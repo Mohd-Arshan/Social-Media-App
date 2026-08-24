24968cb57fef2dc90e36a04ed480274f14afd1dd
Mohd Arshan <arshaan786bijnor0@gmail.com>
2026-08-24 18:50:52 +0530

    Edit Account and Post Done

--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {createPost, getAllPosts, updatePost, deletePost, getRecommendedPosts, getPostsByUserId} = require("../controllers/postController");
+const {createPost, getAllPosts, updatePost, deletePost, getRecommendedPosts, getPostsByUserId, getPostById} = require("../controllers/postController");
 const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
@@ -7,8 +7,9 @@ const router = express.Router();
 router.post('/create',protect,createPost);
 router.get('/getAll',protect,getAllPosts);
 router.patch('/update/:postId',protect,updatePost);
-router.delete('/delete',protect,deletePost);
+router.delete('/delete/:postId',protect,deletePost);
 router.get('/getPostsByUserId/:userId',protect,getPostsByUserId);
 router.get('/recommend-posts',protect,getRecommendedPosts);
+router.get('/getPostById/:postId',protect,getPostById);
 
 module.exports = router;
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const protect = require('../middleware/authMiddleware');
 
-const { getProfileById, getRecommendedProfiles } = require('../controllers/userController');
+const { getProfileById, getRecommendedProfiles, updateProfile } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.get('/profile/:id', getProfileById);
 router.get('/recommend-profiles', protect, getRecommendedProfiles);
+router.patch('/update/:id', protect, updateProfile);
 
 module.exports = router;
